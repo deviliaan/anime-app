bef3634711fbeb3de6d2cae770b0a3175794d823
kai00588 <blankblankboo@gmail.com>
2024-01-22 13:43:06 +0530

    card added

---
+++ src/components/Card.svelte
@@ -0,0 +1,20 @@
+<script>
+    export let anime;
+</script>
+<div class="relative card w-full h-full bg-base-100 shadow-xl pb-2">
+  <figure class="">
+    <img src="{anime?.thumbnail}" alt="Shoes" class="rounded-xl" />
+  </figure>
+  <div class="card-body w-full p-0 items-center text-center">
+    <h2 class="w-full px-2 text-sm card-title truncate">{anime?.title}</h2>
+    <span>Episodes: {anime?.episode}</span>
+    <div class="flex flex-wrap px-2 gap-2">
+      {#each anime.genres.split(',') as genre}
+        <span class="badge badge-primary">{genre}</span>
+      {/each}
+    </div>
+    <div class="absolute top-0 left-0 z-50">
+      <span class="bg-orange-300 rounded-3xl px-2 truncate">{anime.type}</span>
+    </div>
+  </div>
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,12 +2,15 @@
     // @ts-nocheck
     import axios from 'axios'
     import {onMount} from 'svelte'
+	import Card from '../components/Card.svelte';
     export let data;
     const {animes} = data;
 </script>
 
-<div class="bg-orange-300">
-    {#each animes as anime}
-        <h2>{anime?.title}</h2>
-    {/each}
+<div class="">
+    <div class="grid grid-cols-2 p-2 gap-2">
+        {#each animes as anime}
+            <Card anime={anime}/>
+        {/each}
+    </div>
 </div>
